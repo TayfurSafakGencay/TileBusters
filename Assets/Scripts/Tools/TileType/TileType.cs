47d525dc560c3d8e202ec7323ee00897e2e07ae9
TayfurSafakGencay <safak.gencay@gmail.com>
2024-06-25 20:42:02 +0300

    New: Tile Types in Scriptableobject.

---
+++ Assets/Scripts/Tools/TileType/TileType.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Vo;
+
+namespace Tools.TileType
+{
+  [CreateAssetMenu(fileName = "TileTypes", menuName = "Create Tile Data", order = 0)]
+  public class TileType : ScriptableObject
+  {
+    public TileType Instance;
+    
+    [SerializeField]
+    private List<TileSpriteVo> _tiles;
+
+    private void OnEnable()
+    {
+      Instance = this;
+    }
+
+    public List<TileSpriteVo> GetTiles()
+    {
+      return _tiles;
+    }
+  }
+}
